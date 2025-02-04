c36151229a7d2fe499a91f7668ea035fc514e3f0
ScoobyScuit <bminet08@gmail.com>
2025-02-04 16:19:21 +0100

    Ok

--- backend/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DataModule } from './data/data.module';
-import { DatabaseModule } from './database/database.module';
-
-@Module({
-  imports: [DataModule, DatabaseModule],
-})
-export class AppModule {}
--- backend/src/database/database.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DatabaseService } from './database.service';
-
-@Module({
-  providers: [DatabaseService], // Déclare DatabaseService comme un fournisseur
-  exports: [DatabaseService], // Permet aux autres modules d'utiliser DatabaseService
-})
-export class DatabaseModule {}
