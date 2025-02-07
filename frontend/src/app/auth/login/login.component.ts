edc3f3a8c635adb5567c7fa9a34ada5f2d0d2e01
choca64 <chocadu01@gmail.com>
2025-02-07 15:23:53 +0100

    login page

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Ajout de FormsModule ici
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule], // Ajout de FormsModule ici
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+  username: string = '';
+  password: string = '';
+  showPassword: boolean = false;
+  isDarkMode: boolean = false; // ✅ Ajout de cette propriété
 
+  @ViewChild('usernameInput', { static: false }) usernameInput!: ElementRef;
+  @ViewChild('passwordInput', { static: false }) passwordInput!: ElementRef;
+
+  ngAfterViewInit() {
+    this.setupFocusBlurListeners(this.usernameInput.nativeElement);
+    this.setupFocusBlurListeners(this.passwordInput.nativeElement);
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  setupFocusBlurListeners(inputElement: HTMLInputElement) {
+    const parentDiv = inputElement.closest('.input-container');
+
+    inputElement.addEventListener('focus', () => {
+      parentDiv?.classList.add('focus');
+    });
+
+    inputElement.addEventListener('blur', () => {
+      if (inputElement.value === '') {
+        parentDiv?.classList.remove('focus');
+      }
+    });
+  }
+
+  toggleDarkMode(): void {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }
 }
