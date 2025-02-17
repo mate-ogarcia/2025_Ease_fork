18caecac71d71f3aed02c7df26604f1878f980a4
ScoobyScuit <bminet08@gmail.com>
2025-02-17 14:29:53 +0100

    Modif gitignore

--- frontend/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  backendUrl: 'http://localhost:3000/data', // URL de l'API backend
-  searchUrl: 'http://localhost:3000/request-handler/search', // URL de l'API backend
-  productsURL : 'http://localhost:3000/products',
-  globalBackendUrl: 'http://localhost:3000',
-  authBackendUrl: 'http://localhost:3000/auth',
-};
