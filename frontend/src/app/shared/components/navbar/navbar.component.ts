400030c1b2e47944e16c2965e62dfd02221f4c4c
choca64 <chocadu64@gamil.com>
2025-04-17 11:45:11 +0200

    more corrections

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -97,7 +97,7 @@ export class NavbarComponent implements OnInit {
    */
   @HostListener('window:resize', ['$event'])
   checkScreenSize() {
-    this.isMobile = window.innerWidth <= 850;
+    this.isMobile = window.innerWidth <= 1250;
   }
 
   /**
